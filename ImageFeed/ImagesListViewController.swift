19abb908310529cb0be02e13ea0cb596272fb606
Artem Chebaturov <powerslave42@yandex.ru>
2023-07-12 17:39:28 +0300

    начал реализовывать проект: создана + настроена тэйблвью

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
