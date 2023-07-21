a393740c682cf6cfb252a23d263cccbfc7035e85
Artem Chebaturov <powerslave42@yandex.ru>
2023-07-21 18:32:20 +0300

    раскидал констрейнты в экране логина

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -5,4 +5,20 @@ import UIKit
 final class ProfileViewController: UIViewController {
     
     
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logOutButton: UIButton!
+    
+    @IBAction func didTapLogOutButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
 }
