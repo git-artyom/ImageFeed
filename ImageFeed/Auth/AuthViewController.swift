7606cd0bd77b41a248e1d386efd84376b7472deb
Artem Chebaturov <powerslave42@yandex.ru>
2023-08-02 21:48:19 +0300

    начал реализовывать задачи 10 спринта: создал экран авторизации

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Чебатуров on 02.08.2023.
+//
+
+import Foundation
+
+
+class AuthViewController {
+    
+    
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Артем Чебатуров on 02.08.2023.
+//
+
+import Foundation
+
+
+let AccessKey = "orbCtm0t7AGUkZsz6MEkUQdLAfGc9gVD-mrAfBMdxVc"
+let SecretKey = "w_3cz0xK8Z-qAtSDH9VD7CMmLLvKMyAMj2NRwK6Akcg"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
